a64f84272643f54e35212ce92c92474e0befcf8a
Kai Aoyama <sauna.kaikai@gmail.com>
2022-02-06 16:49:43 +0900

    ソーシャルログイン成功

--- FBSocialLogin/ViewController.swift
+++ FBSocialLogin/ViewController.swift
@@ -5,12 +5,20 @@
 //  Created by ヘパリン類似物質 on 2022/02/06.
 //
 
+
+// Swift // // ファイルのヘッダーにこれを追加します。例: ViewController.swift import FBSDKLoginKit // 本文クラスViewControllerにこれを追加します:UIViewController { override func viewDidLoad() { super.viewDidLoad() let loginButton = FBLoginButton() loginButton.center = view.center view.addSubview(loginButton) } }
+
+
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        view.addSubview(loginButton)
         // Do any additional setup after loading the view.
     }
 
